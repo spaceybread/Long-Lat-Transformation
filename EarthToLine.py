61afc86527e48a916936d1d663401ae327ed4f01
spaceybread <spaceybread@gmail.com>
2024-06-21 14:22:28 -0500

    Added calculations for EarthToLine.py

--- EarthToLine.py
+++ EarthToLine.py
@@ -12,5 +12,27 @@ def earthToImage(lat, long):
     F_dr = 1 #
     # =======================================
     
+    colat = math.pi / 2 - F_dr * lat
+    in_lon = -1 * F_dr * lon
+    
+    if in_lon <= -1 * math.pi:
+        in_lon += 2 * math.pi
+    
+    if in_lon >= math.pi:
+        in_lon += -2 * math.pi
+        
+    rad = R * math.tan(std_colat) * ( math.tan(colat / 2) / math.tan(std_colat / 2) ) ** (math.cos(std_colat))
+    
+    theta = in_lon - std_long
+    
+    if theta <= -1 * math.pi:
+        theta += 2 * math.pi
+    if theta >= math.pi:
+        theta += -2 * math.pi
+    
+    theta *= math.cos(std_colat)
+    
+    image_line = pole_line + rad * math.cos(theta)/scale
+    image_element = pole_element + rad * math.sin(theta)/scale
     
     return (image_line, image_element)
