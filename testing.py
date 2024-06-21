872f727eae26c8c4d465be39024a0879f3c6aacf
spaceybread <spaceybread@gmail.com>
2024-06-21 16:04:32 -0500

    Testing coeffs

--- testing.py
+++ testing.py
@@ -1,13 +1,16 @@
 from EarthToLine import earthToImage
 from LineToEarth import imageToEarth
+from math import fabs
+from random import randint
 
-x, y = 3, 5
+for _ in range(5):
+    x, y = randint(1, 2000), randint(1, 2000)
 
-a, b = imageToEarth(x, y)
-c, d = earthToImage(a, b)
+    a, b = imageToEarth(x, y)
+    c, d = earthToImage(a, b)
 
-print(x, y)
-print(a, b)
-print(c, d)
-print(c / x, d / y)
-
+    print(x, y)
+    print(a, b)
+    print(c, d)
+    print(c / x, d / y, fabs(c / x - d / y))
+    print("==================")
