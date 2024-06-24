6da860d21af95ac242b969cc3e72071bae75dda3
spaceybread <spaceybread@gmail.com>
2024-06-24 10:19:01 -0500

    checks for more calculated stuff

--- testing.py
+++ testing.py
@@ -6,7 +6,7 @@ from random import randint
 
 os.system('clear')
 print("================================================================")
-for _ in range(12):
+for _ in range(8):
     x, y = randint(1, 100000), randint(1, 100000)
 
     a, b = imageToEarth(x, y)
@@ -16,4 +16,7 @@ for _ in range(12):
     print(a, b)
     print(c, d)
     print(c / x, d / y, fabs(c / x - d / y))
+    print(x / c, y / d, fabs(x / c - y / d))
+    print(x / c * c, x / c * d)
+    print(y / d * c, y / d * d)
     print("================================================================")
